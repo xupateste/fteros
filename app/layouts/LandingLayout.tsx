162b15120a8f3bdaf28a76b5c86d33a338cabc16
Christian Barreto <christianbarreto@MacBook-Pro-de-Christian.local>
2023-06-07 14:53:52 -0500

    meta001

--- app/layouts/LandingLayout.tsx
+++ app/layouts/LandingLayout.tsx
@@ -43,24 +43,24 @@ const LandingLayout: React.FC = ({children}) => (
                   }}
       />
       <script
-        // dangerouslySetInnerHTML={{
-        //   __html: `!function(f,b,e,v,n,t,s)
-        //   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        //   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        //   if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        //   n.queue=[];t=b.createElement(e);t.async=!0;
-        //   t.src=v;s=b.getElementsByTagName(e)[0];
-        //   s.parentNode.insertBefore(t,s)}(window, document,'script',
-        //   'https://connect.facebook.net/en_US/fbevents.js');
-        //   fbq('init', '${tenant.pixel}');
-        //   fbq('track', 'PageView');`,
-        // }}
+        dangerouslySetInnerHTML={{
+          __html: `!function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1291712908092962');
+          fbq('track', 'PageView');`,
+        }}
       />
       <noscript
-        // dangerouslySetInnerHTML={{
-        //   __html: `<img height="1" width="1" style="display:none"
-        //   src="https://www.facebook.com/tr?id=${tenant.pixel}&ev=PageView&noscript=1" />`,
-        // }}
+        dangerouslySetInnerHTML={{
+          __html: `<img height="1" width="1" style="display:none"
+          src="https://www.facebook.com/tr?id=1291712908092962&ev=PageView&noscript=1" />`,
+        }}
       />
       <meta content={META.title} name="twitter:title" />
       <meta content={META.description} name="twitter:description" />
