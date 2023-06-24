90ee313c89cc87c8ef3992625d3d319813ed5d42
Christian Barreto <christianbarreto@MacBook-Pro-de-Christian.local>
2023-06-23 23:47:37 -0500

    meta008

--- i18n/selectors.ts
+++ i18n/selectors.ts
@@ -21,3 +21,34 @@ export function formatPrice(price: number, format: "locale" | "iso" = "locale") 
     }
   }
 }
+
+
+export function formatSymbol(format: "locale" | "iso" = "locale") {
+  const locale = i18n?.language || LOCALES[DEFAULT_COUNTRY];
+  const result = Object.entries(LOCALES).find(([, _locale]) => locale === _locale);
+
+  const country = result?.[0] || DEFAULT_COUNTRY;
+
+  function getCurrencySymbol (locale, currency) {
+    return (0).toLocaleString(
+      locale,
+      {
+        style: 'currency',
+        currency: currency,
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0
+      }
+    ).replace(/\d/g, '').trim()
+  }
+
+  switch (format) {
+    case "iso": {
+      return `${CURRENCIES[country]}`;
+    }
+
+    case "locale":
+    default: {
+      return getCurrencySymbol(LOCALES[country], CURRENCIES[country])
+    }
+  }
+}
--- ui/inputs/Price.tsx
+++ ui/inputs/Price.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {InputGroup, InputLeftElement, InputGroupProps, InputProps} from "@chakra-ui/core";
 
 import Input from "./Input";
+import {formatSymbol} from "~/i18n/selectors";
 
 interface Props extends Omit<InputGroupProps, "children" | "onChange"> {
   name?: InputProps["name"];
@@ -16,7 +17,7 @@ const Price: React.FC<Props> = React.forwardRef(
   ({name, value, placeholder, onChange, rounded, inputProps = {}, ...props}, ref) => (
     <InputGroup {...props}>
       <InputLeftElement
-        children="$"
+        children={formatSymbol()}
         backgroundColor="transparent"
         paddingX={0}
         pointerEvents="none"
@@ -28,7 +29,7 @@ const Price: React.FC<Props> = React.forwardRef(
         name={name}
         placeholder={placeholder}
         rounded={rounded}
-        style={{paddingLeft: "2rem"}}
+        style={{paddingLeft: "2.5rem"}}
         type="number"
         value={value}
         variant="filled"
