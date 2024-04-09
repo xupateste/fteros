1936e418364d1d405a4ff3f3b0e390c96101659b
ha <some@email.com>
2024-04-08 19:48:31 -0500

    fix style added cart

--- ui/inputs/StepperPackedLanding/StepperPackedLanding.tsx
+++ ui/inputs/StepperPackedLanding/StepperPackedLanding.tsx
@@ -50,7 +50,7 @@ const StepperPackedLanding: React.FC<Props> = ({
   }
 
   return (
-    <Stack isInline alignItems="center" spacing={0} mx={2} {...props}>
+    <Stack rounded="lg" isInline alignItems="center" spacing={0} mx={2} backgroundColor="primary.200" {...props}>
       {value && (
         <IconButton
           size="sm"
